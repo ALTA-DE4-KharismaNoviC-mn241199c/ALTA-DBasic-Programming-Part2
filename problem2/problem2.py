c9f497cdd0ca20b43c88b75458b805b5cc732129
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:26:58 +0700

    Add files via upload

---
+++ problem2/problem2.py
@@ -0,0 +1,10 @@
+#input
+alas = 20.0
+tinggi = 25.0
+
+#output
+input_alas = input("Enter input_alas =  ")
+input_tinggi = input("Enter input_tinggi =  ")
+area = (alas * tinggi) /2
+
+print("Triangle Area =  ", round(area,2))
