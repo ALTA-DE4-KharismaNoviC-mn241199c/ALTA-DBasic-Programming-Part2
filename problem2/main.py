8b8d644437e2f12ff4df97a5385aed311ce0e484
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:25:12 +0700

    update and commit

--- problem2/main.py
+++ problem2/main.py
@@ -1,6 +1,10 @@
-# input
+#input
 alas = 20.0
 tinggi = 25.0
 
+#output
+input_alas = input("Enter input_alas =  ")
+input_tinggi = input("Enter input_tinggi =  ")
+area = (alas * tinggi) /2
 
-# kode disini
+print("Triangle Area =  ", round(area,2))
