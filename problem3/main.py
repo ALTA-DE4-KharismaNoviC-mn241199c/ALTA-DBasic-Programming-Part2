71e4e5940efb1a1cef37ff875cf81a8f54242d26
Kharisma Novi C <168548312+rismanovic24@users.noreply.github.com>
2024-06-21 22:25:44 +0700

    update and commit

--- problem3/main.py
+++ problem3/main.py
@@ -1,6 +1,11 @@
-# input
+#input
 T = 20.0
 r = 4.0
 
+#output
+input_T = input("Enter input_T =  ")
+input_r = input("Enter input_r =  ")
+phi = 3.14
+area = (2 * phi * r * r) + (2 * phi * r * T)
 
-# kode disini
+print("Tube Area =  ", round(area,2))
